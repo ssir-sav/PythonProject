61aa3b0ae9fea79fee6612bf3c1f36aaea9aa530
savannahp <qsavy07@gmail.com>
2025-01-18 14:03:59 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print('hi! im sav:3')
+print('how are you??:D')
+happy = 'happy, happy, happy! (do do do do do do)'
+print(happy)
+print(len(happy))
+print(happy[0])
+print(happy[0:5])
+print(happy.upper())
+print(happy.count('happy'))
+print(happy.find('do'))
+print(happy.find('heheh'))
+happy = happy.replace('do do do do do do', 'do do do do do doom')
+print(happy)
+greeting = 'hello'
+name = 'player 1'
+message = f'{greeting}, {name.upper()}! hru???'
+print(message)
